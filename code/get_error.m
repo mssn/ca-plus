f84422309690dadb4c76c7caa52ae131d01fcdc7
qianru <cachymail@gmail.com>
2023-04-30 17:02:00 -0400

    Delete get_error.m

--- code/get_error.m
+++
@@ -1,6 +0,0 @@
-function [error] = get_error(est_H, Orig_H)
-est_norm = mean(mean((abs(est_H))));
-gt_norm = mean(mean((abs(Orig_H))));
-error = 10*log10(est_norm/gt_norm);
-fprintf('===\nest %f orig %f error %f db\n===\n',est_norm, gt_norm,error);
-end
